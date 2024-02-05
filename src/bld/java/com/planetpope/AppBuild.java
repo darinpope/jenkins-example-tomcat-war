b3f7e8898601be1a6c7069ec34614eb33e18dcbd
Darin Pope <darin@planetpope.com>
2024-02-05 16:17:19 -0500

    apply updates

--- src/bld/java/com/planetpope/AppBuild.java
+++ src/bld/java/com/planetpope/AppBuild.java
@@ -21,12 +21,12 @@ public class AppBuild extends WebProject {
         scope(compile)
             .include(dependency("com.uwyn.rife2", "rife2", version(1,7,3)));
         scope(test)
-            .include(dependency("org.jsoup", "jsoup", version(1,16,2)))
-            .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,10,1)))
-            .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1,10,1)));
+            .include(dependency("org.jsoup", "jsoup", version(1,17,2)))
+            .include(dependency("org.junit.jupiter", "junit-jupiter", version(5,10,2)))
+            .include(dependency("org.junit.platform", "junit-platform-console-standalone", version(1,10,2)));
         scope(standalone)
-            .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10", version(12,0,5)))
-            .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10-servlet", version(12,0,5)))
+            .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10", version(12,0,6)))
+            .include(dependency("org.eclipse.jetty.ee10", "jetty-ee10-servlet", version(12,0,6)))
             .include(dependency("org.slf4j", "slf4j-simple", version(2,0,11)));
 
         precompileOperation()
